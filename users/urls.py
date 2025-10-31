758a581053cc28bf50b895fcfd4c2f38421dcfc7
Dennis Tereshkow <tereshkowdennis@gmail.com>
2025-10-31 14:22:31 +0300

    feat: update project urls

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('profile/', views.profile_view, name='profile'),
-    path('delete-account/', views.delete_account_view, name='delete-account'),
+    path('register/', views.register, name='users-register'),
+    path('profile/', views.profile, name='users-profile'),
+    path('profile/update/', views.update_profile, name='users-update-profile'),
+    path('delete-account/', views.delete_account, name='users-delete-account'),
 ]
